9270388cb01ddd73ad52262071b6f66f1f6cfb07
GedorN <gedor.silvaneto@gmail.com>
2025-11-18 16:58:06 -0300

    Swagger improvement v1

--- src/employees/dto/create-employee.dto.ts
+++ src/employees/dto/create-employee.dto.ts
@@ -1,18 +1,23 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsNumber, IsString, Min, MinLength } from 'class-validator';
 
 export class CreateEmployeeDto {
+    @ApiProperty({ example: 'John Doe' })
     @IsString()
     @MinLength(2)
     name: string;
 
+    @ApiProperty({ example: 'Backend Developer' })
     @IsString()
     @MinLength(2)
     role: string
 
+    @ApiProperty({ example: 12000 })
     @IsNumber()
     @Min(0)
     salary: number;
 
+    @ApiProperty({ example: true })
     @IsBoolean()
     isActive: boolean;
 
--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -2,27 +2,34 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe } from 
 import { EmployeesService } from './employees.service';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+
 
 @Controller('employees')
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Creates a new employee' })
+  @ApiResponse({ status: 201, description: 'Employee successfully created' })
   create(@Body() createEmployeeDto: CreateEmployeeDto) {
     return this.employeesService.create(createEmployeeDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Lists all employees' })
   findAll() {
     return this.employeesService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Search a employee by ID' })
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.employeesService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update a employee' })
   update(
     @Param('id', ParseIntPipe) id: number, 
     @Body() updateEmployeeDto: UpdateEmployeeDto
@@ -31,6 +38,7 @@ export class EmployeesController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Remove a employee' })
   remove(@Param('id', ParseIntPipe) id: number) {
     this.employeesService.remove(id);
     return { message: `Employee ${id} removed` };
